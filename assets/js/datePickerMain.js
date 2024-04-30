c6ee03949d480337756dc34de4fdf566acfae956
shantocnits <shanto.cnits@gmail.com>
2024-04-30 13:34:46 +0600

    credit card start by shanto

---
+++ assets/js/datePickerMain.js
@@ -0,0 +1,4 @@
+var datepicker1 = new Datepicker("#datepicker1");
+var datepicker2 = new Datepicker("#datepicker2");
+var datepicker3 = new Datepicker("#datepicker3");
+var datepicker4 = new Datepicker("#datepicker4");
