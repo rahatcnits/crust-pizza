5c75f64d141d0068a148f97f895a9625694689fc
rahatcnits <rahatcnits@gmail.com>
2024-05-02 20:09:38 +0600

    checkout page completed

--- assets/js/addTrip.js
+++ assets/js/addTrip.js
@@ -11,3 +11,19 @@ selectTripItems.forEach(function (selectTripItem) {
     selectTripItem.classList.add("active");
   });
 });
+
+// add card
+
+const cardAdd = document.querySelector(".add_card_content");
+const cardGift = document.querySelector(".gift_card_content");
+function hideShow(val) {
+  if (val === 1) {
+    cardAdd.classList.add("active");
+    cardGift.classList.remove("active");
+  }
+
+  if (val === 2) {
+    cardAdd.classList.remove("active");
+    cardGift.classList.add("active");
+  }
+}
